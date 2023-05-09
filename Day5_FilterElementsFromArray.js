8f2671e1bbb0f4082c3e3a2e0b175d3f954a4c13
Nithin Chandran P <p.nithinc@gmail.com>
2023-05-09 17:02:49 +0530

    Filter elements from array

---
+++ Day5_FilterElementsFromArray.js
@@ -0,0 +1,11 @@
+var filterElements = function(arr, fn){
+    const outputArray = []
+
+    for (let i =0;i<arr.length;i++){
+        if(fn(arr[i],i)){
+            outputArray.push(arr[i])
+        }
+    }
+
+    return outputArray
+}
