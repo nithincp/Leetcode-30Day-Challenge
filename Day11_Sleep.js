45323fc772f458d98ef12f462f937f06731e3807
Nithin Chandran P <p.nithinc@gmail.com>
2023-05-15 12:01:01 +0530

    Async,await,Promise

--- Day11_Sleep.js
+++ Day11_Sleep.js
@@ -6,3 +6,14 @@ async function sleep(millis){
 
 // let t = Date.now()
 // sleep(1000).then(() => console.log(Date.now() - t))
+
+// async function sleep(millis) {
+//     await new Promise(resolve => setTimeout(resolve, millis));
+//   }
+  
+//   const promise = sleep(1000);
+//   promise.then(() => {
+//     console.log('Promise resolved!');
+//   });
+  
+//   console.log('After calling sleep function');
