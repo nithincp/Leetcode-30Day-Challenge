85ea506d6bb734450de188910ff833a1414cd7bf
Nithin Chandran P <p.nithinc@gmail.com>
2023-05-16 21:31:49 +0530

    Using async and await

--- Day12_PromiseTimeLimit.js
+++ Day12_PromiseTimeLimit.js
@@ -1,21 +1,36 @@
 //Leetcode Problem - 2637
 
+// var timeLimit = async function(fn,t){
+
+//   return async function(...args){
+//     return new Promise((resolve,reject) => {
+//       const id = setTimeout(() => reject('Time Limit Exceeded'), t)
+
+//       fn(...args)
+//         .then((res) => {
+//           resolve(res)
+//         })
+//         .catch((error) => reject(error))
+//         .finally(() => clearTimeout(id))
+//     })
+//   }
+// }
+
+
+
 var timeLimit = async function(fn,t){
-
+  
   return async function(...args){
-    return new Promise((resolve,reject) => {
-      const id = setTimeout(() => reject('Time Limit Exceeded'), t)
-
-      fn(...args)
-        .then((res) => {
-          resolve(res)
-        })
-        .catch((error) => reject(error))
-        .finally(() => clearTimeout(id))
+    return new Promise(async (resolve,reject) => {
+      try {
+        const res = await fn(...args)
+        resolve(res)
+      } catch (error) {
+        reject(error)
+      }
     })
   }
 }
-
 
 
 
