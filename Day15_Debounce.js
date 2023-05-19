121964e3680b824594f3836b5dda5a6d66866878
Nithin Chandran P <p.nithinc@gmail.com>
2023-05-19 17:51:56 +0530

    Debounce

---
+++ Day15_Debounce.js
@@ -0,0 +1,17 @@
+//Leetcode Problem - 2627
+
+var debounce = function (fn, t) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => fn(inputs), executionTime);
+  };
+};
+
+// let start = Date.now();
+// function log(...inputs) {
+//   console.log([Date.now() - start, inputs]);
+// }
+// const dlog = debounce(log, 50);
+// setTimeout(() => dlog(1), 50);
+// setTimeout(() => dlog(2), 75);
